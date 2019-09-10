0da8ec4b8cbaa760d2c7859b340166d79cffad5b
tazkaly@gmail.com <tazkaly@gmail.com>
2019-09-10 22:40:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/therehabapp/Introduction.java
@@ -0,0 +1,15 @@
+package com.example.therehabapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Introduction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.introduction_view);
+    }
+
+}
---
+++ app/src/main/java/com/example/therehabapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.therehabapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button nextBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nextBtn= (Button) findViewById(R.id.enter_btn);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Introduction.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
