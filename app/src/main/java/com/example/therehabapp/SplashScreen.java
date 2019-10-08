01866030f88febf1dcc8dea7866e14fac403bbb2
tazkaly@gmail.com <tazkaly@gmail.com>
2019-10-08 15:53:02 +0200

    commit: splash screen created

--- app/src/main/java/com/example/therehabapp/SplashScreen.java
+++ app/src/main/java/com/example/therehabapp/SplashScreen.java
@@ -1,14 +1,28 @@
 package com.example.therehabapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private final int SPLASH_SCREEN_LENGTH = 5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launcher_view);
+
+
+       new Handler().postDelayed(new Runnable(){
+           @Override
+           public void run(){
+               Intent mainIntent= new Intent(SplashScreen.this, WelcomeNote.class);
+               SplashScreen.this.startActivity(mainIntent);
+               SplashScreen.this.finish();
+           }
+       },SPLASH_SCREEN_LENGTH);
     }
 }
