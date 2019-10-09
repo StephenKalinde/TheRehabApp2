75af0afb5017ad667345195870e74adb08865e47
tazkaly@gmail.com <tazkaly@gmail.com>
2019-10-09 20:16:55 +0200

    commit: fragments filled in

--- app/src/main/java/com/example/therehabapp/Home.java
+++ app/src/main/java/com/example/therehabapp/Home.java
@@ -6,8 +6,6 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
